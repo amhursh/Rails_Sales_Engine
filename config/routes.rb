31232c5ab1ebe4f9fb010231884e64234a68c35a
amhursh <amhursh@gmail.com>
2017-10-03 16:41:56 -0600

    Add items search controller and find/show

--- config/routes.rb
+++ config/routes.rb
@@ -21,6 +21,9 @@ Rails.application.routes.draw do
         get '/find_all', to: 'search#index'
         get '/random', to: 'random#show'
       end
+      namespace :items do
+        get '/find', to: 'search#show'
+      end
     	resources :transactions, only: [:index, :show]
       resources :merchants, only: [:index, :show]
       resources :invoices, only: [:index, :show]

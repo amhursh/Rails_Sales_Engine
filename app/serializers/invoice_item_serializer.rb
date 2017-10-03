7adb63af397a761628f2d5ace52cc2d1922cb363
jayphodges <jayphodges@gmail.com>
2017-10-03 16:36:45 -0600

    Added unit price to invoice item serializer

--- app/serializers/invoice_item_serializer.rb
+++ app/serializers/invoice_item_serializer.rb
@@ -1,3 +1,8 @@
 class InvoiceItemSerializer < ActiveModel::Serializer
   attributes :id, :item_id, :invoice_id, :quantity, :unit_price
+
+  def unit_price
+    (object.unit_price / 100.0).to_s
+  end
+
 end

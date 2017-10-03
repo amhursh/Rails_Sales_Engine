2c78a5535bc3b2bcb6b85790074493dd9c10ad32
amhursh <amhursh@gmail.com>
2017-10-03 01:07:59 -0600

    Add test for find

--- spec/requests/api/v1/invoices_request_spec.rb
+++ spec/requests/api/v1/invoices_request_spec.rb
@@ -30,4 +30,48 @@ describe "Invoices API" do
 		expect(invoice["id"]).to_not eq invoice1.id
 		expect(invoice["id"]).to_not eq invoice3.id
 	end
+
+	it "can find individual invoice by params" do
+		customer1, customer2 = create_list(:customer, 2)
+		merchant1, merchant2 = create_list(:merchant, 2)
+
+		invoice1 = create(:invoice,
+											customer: customer1, 
+											merchant: merchant1, 
+											created_at: "2017-05-20 00:00:00 UTC",
+											updated_at: "2017-05-21 00:00:00 UTC",
+											status: "shipped"
+											)
+		invoice2 = create(:invoice,
+											customer: customer2,
+											merchant: merchant2,
+											created_at: "2017-04-20 00:00:00 UTC",
+											updated_at: "2017-04-21 00:00:00 UTC",
+											status: "cancelled"
+											)
+		invoice3 = create(:invoice, 
+											customer: customer1,
+											merchant: merchant2,
+											created_at: "2017-03-20 00:00:00 UTC",
+											updated_at: "2017-03-21 00:00:00 UTC",
+											status: "shipped"
+											)
+
+		get "/api/v1/invoices/find?#{invoice1.created_at}"
+
+		invoice = JSON.parse(response.body)
+
+		expect(invoice["id"]).to eq invoice1.id
+
+		get "/api/v1/invoices/find?#{invoice2.updated_at}"
+
+		invoice = JSON.parse(response.body)
+
+		expect(invoice["id"]).to eq invoice2.id
+
+		get "/api/v1/invoices/find?#{invoice3.status}"
+
+		expect(invoice["id"]).to eq invoice1.id
+		expect(invoice.count).to eq 1
+	end
 end

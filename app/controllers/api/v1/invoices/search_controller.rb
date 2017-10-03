8b9301d032e4a942ff67a450ee229efde8e2c2b1
amhursh <amhursh@gmail.com>
2017-10-03 01:44:20 -0600

    Add find/show action and logic

--- app/controllers/api/v1/invoices/search_controller.rb
+++ app/controllers/api/v1/invoices/search_controller.rb
@@ -1,3 +1,13 @@
 class Api::V1::Invoices::SearchController < ApplicationController
 
+	def show
+		render json: Invoice.find_by(invoice_params)
+	end
+
+	private
+
+	def invoice_params
+		params.permit(:id, :status, :created_at, :updated_at, :customer_id, :merchant_id)
+	end
+
 end

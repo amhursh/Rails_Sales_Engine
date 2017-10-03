0bff984fb1557fbf3701bdf4631ccb5832f3b3fb
amhursh <amhursh@gmail.com>
2017-10-03 15:35:15 -0600

    Add test for items show

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -13,4 +13,21 @@ describe "Items API" do
     expect(response).to be_success
     expect(items.count).to eq 3
   end
+
+  it "can get one item by its id" do
+    merchant = create(:merchant)
+
+    item1, item2, item3 = create_list(:item, 3, merchant: merchant)
+
+    get "/api/v1/items/#{item3.id}"
+
+    item = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(item["id"]).to eq item3.id
+    expect(item["name"]).to eq item3.name
+    expect(item["description"]).to eq item3.description
+    expect(item["merchant_id"]).to eq merchant.id
+    expect(item["unit_price"]).to eq item3.unit_price
+  end
 end

7288894de51a8ec750bab5b3d544f3733e7e0b0c
amhursh <amhursh@gmail.com>
2017-10-03 16:24:46 -0600

    Add test for item find

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -30,4 +30,52 @@ describe "Items API" do
     expect(item["merchant_id"]).to eq merchant.id
     expect(item["unit_price"]).to eq (item3.unit_price / 100.0).to_s
   end
+
+  it "can find individual items by params" do
+    merchant = create(:merchant)
+    merchant2 = create(:merchant)
+
+    item1 = create(:item,
+                   name: "Aaron",
+                   description: "something",
+                   unit_price: 3000,
+                   merchant_id: merchant.id,
+                   )
+    item2 = create(:item,
+                   name: "JP",
+                   description: "coolbeans",
+                   unit_price: 5000,
+                   merchant_id: merchant2.id,
+                   )
+    item3 = create(:item,
+                   name: "This guy",
+                   description: "whatthehell",
+                   unit_price: 9000,
+                   merchant_id: merchant.id,
+                   )
+
+    get "/api/v1/items/find?id=#{item1.id}"
+
+    item = JSON.parse(response.body)
+
+    expect(item["id"]).to eq item1.id
+
+    get "/api/v1/items/find?name=#{item2.name}"
+
+    item = JSON.parse(response.body)
+
+    expect(item["name"]).to eq item2.name
+
+    get "/api/v1/items/find?description=#{item3.description}"
+
+    item = JSON.parse(response.body)
+
+    expect(item["description"]).to eq item3.description
+
+    get "/api/v1/items/find?unit_price=#{item3.merchant_id}"
+
+    item = JSON.parse(response.body)
+
+    expect(item["id"]).to eq item1.id
+  end
 end

82880117c61d31fd9268e700649b0bab91d5a309
amhursh <amhursh@gmail.com>
2017-10-04 03:16:14 -0600

    Add token to json response

--- app/controllers/api/v1/merchants/revenue_controller.rb
+++ app/controllers/api/v1/merchants/revenue_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::Merchants::RevenueController < ApplicationController
 
   def show
-    render json: Merchant.total_revenue_for_date(params[:date])
+    render json: { "total" => Merchant.total_revenue_for_date(params[:date]) }
   end
 
 end

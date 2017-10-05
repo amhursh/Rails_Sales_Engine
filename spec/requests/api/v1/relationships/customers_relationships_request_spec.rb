57eb27ee984833e033f7e2a9d622672aca00f4dc
amhursh <amhursh@gmail.com>
2017-10-05 00:11:26 -0600

    Add customer transactions endpoint test

--- spec/requests/api/v1/relationships/customers_relationships_request_spec.rb
+++ spec/requests/api/v1/relationships/customers_relationships_request_spec.rb
@@ -13,4 +13,21 @@ describe "Customers Relationships API" do
     expect(response).to be_success
     expect(invoices.count).to eq 13
   end
+
+  it "returns a collection of associated transactions" do
+    customer = create(:customer)
+    merchant = create(:merchant)
+    create_list(:invoice, 5, customer: customer, merchant: merchant)
+
+    customer.invoices.each do |invoice|
+      2.times { create(:transaction, invoice: invoice) }
+    end
+
+    get "/api/v1/customers/#{customer.id}/transactions"
+
+    transactions = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(transactions.count).to eq 10
+  end
 end

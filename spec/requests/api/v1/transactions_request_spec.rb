726118dfa005e3b710f2ffbcf886c9f9827c6dc8
jayphodges <jayphodges@gmail.com>
2017-10-04 22:38:36 -0600

    Completed testing of basic endpoints and additional testing of invoices relationships

--- spec/requests/api/v1/transactions_request_spec.rb
+++ spec/requests/api/v1/transactions_request_spec.rb
@@ -34,4 +34,81 @@ describe "Transactions API" do
     end
   end
 
+  context "GET /transactions/find?params" do
+    it "returns a single transaction from params" do
+      customer = create(:customer)
+      merchant = create(:merchant)
+      invoice1 = create(:invoice, merchant_id: merchant.id, customer_id: customer.id)
+      invoice2 = create(:invoice, merchant_id: merchant.id, customer_id: customer.id)
+      transaction1 = create(:transaction, invoice_id: invoice1.id)
+      transaction2 = create(:transaction, invoice_id: invoice2.id)
+
+
+      get "/api/v1/transactions/find?id=#{transaction1.id}"
+
+      transaction = JSON.parse(response.body)
+
+      expect(response).to be_success
+      expect(transaction["id"]).to eq(transaction1.id)
+
+      get "/api/v1/transactions/find?invoice_id=#{transaction2.invoice_id}"
+
+      transaction = JSON.parse(response.body)
+
+      expect(response).to be_success
+      expect(transaction["id"]).to eq(transaction2.id)
+    end
+  end
+
+  context "GET /transactions/find_all?params" do
+    it "returns a multiple transactions from params" do
+      customer = create(:customer)
+      merchant = create(:merchant)
+      invoice1 = create(:invoice, merchant_id: merchant.id, customer_id: customer.id)
+      invoice2 = create(:invoice, merchant_id: merchant.id, customer_id: customer.id)
+      transaction1 = create(:transaction, invoice_id: invoice1.id)
+      transaction2 = create(:transaction, invoice_id: invoice2.id)
+      transaction3 = create(:transaction, invoice_id: invoice2.id)
+
+
+      get "/api/v1/transactions/find_all?id=#{transaction1.id}"
+
+      transaction = JSON.parse(response.body)
+
+      expect(response).to be_success
+      expect(transaction.first["id"]).to eq(transaction1.id)
+
+      get "/api/v1/transactions/find_all?invoice_id=#{transaction2.invoice_id}"
+
+      transaction = JSON.parse(response.body)
+
+      expect(response).to be_success
+      expect(transaction.count).to eq(2)
+
+      expect(transaction.first["id"]).to eq(transaction2.id)
+      expect(transaction.second["id"]).to eq(transaction3.id)
+    end
+  end
+
+  context "GET /transactions/random" do
+    it "returns a random transaction" do
+      customer = create(:customer)
+      merchant = create(:merchant)
+      invoice1 = create(:invoice, merchant_id: merchant.id, customer_id: customer.id)
+      invoice2 = create(:invoice, merchant_id: merchant.id, customer_id: customer.id)
+      transaction1 = create(:transaction, invoice_id: invoice1.id)
+      transaction2 = create(:transaction, invoice_id: invoice2.id)
+
+
+      get "/api/v1/transactions/random"
+
+      transaction = JSON.parse(response.body)
+
+      expect(response).to be_success
+      # binding.pry
+      # expect(transaction["id"]).to eq(transaction1.id || transaction2.id)
+    end
+  end
+
+
 end

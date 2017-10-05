458c6b5416a363359d2e1793c2cb01085e95c3a2
amhursh <amhursh@gmail.com>
2017-10-05 11:21:40 -0600

    Add customer pending invoices route

--- config/routes.rb
+++ config/routes.rb
@@ -51,6 +51,7 @@ Rails.application.routes.draw do
         get '/most_revenue', to: 'top_revenue#index'
         get '/:id/revenue', to: 'revenue#index'
         get '/:id/favorite_customer', to: 'favorite_customer#show'
+        get '/:id/customers_with_pending_invoices', to: 'pending_customers#index'
         get '/:id/items', to: 'items#index'
         get '/:id/invoices', to: 'invoices#index'
       end

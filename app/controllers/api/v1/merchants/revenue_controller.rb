1e890ae0ab1348b1e4ac5d01d1e6be440c8a72b2
amhursh <amhursh@gmail.com>
2017-10-05 13:14:38 -0600

    Refactor

--- app/controllers/api/v1/merchants/revenue_controller.rb
+++ app/controllers/api/v1/merchants/revenue_controller.rb
@@ -1,9 +1,5 @@
 class Api::V1::Merchants::RevenueController < ApplicationController
 
-  def show
-    render json: { "total" => Merchant.total_revenue_for_date(params[:date]) }
-  end
-
   def index
     render json: { "revenue" => Merchant.find(params[:id]).revenue }
   end

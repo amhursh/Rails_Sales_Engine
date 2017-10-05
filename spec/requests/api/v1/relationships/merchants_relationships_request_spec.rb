5852843a0ea11db29eaf2ffeac088efb8146362b
amhursh <amhursh@gmail.com>
2017-10-04 23:39:50 -0600

    Add merchants invoice test

--- spec/requests/api/v1/relationships/merchants_relationships_request_spec.rb
+++ spec/requests/api/v1/relationships/merchants_relationships_request_spec.rb
@@ -7,9 +7,22 @@ describe "Merchant Relationships API" do
 
     get "/api/v1/merchants/#{merchant.id}/items"
 
-    merchants = JSON.parse(response.body)
+    items = JSON.parse(response.body)
 
     expect(response).to be_success
-    expect(merchants.count).to eq 10
+    expect(items.count).to eq 10
+  end
+
+  it "returns a collection of invoices associated with a merchant" do
+    merchant = create(:merchant)
+    customer = create(:customer)
+    create_list(:invoice, 10, customer: customer, merchant: merchant)
+
+    get "/api/v1/merchants/#{merchant.id}/invoices"
+
+    invoices = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(invoices.count).to eq 10
   end
 end
